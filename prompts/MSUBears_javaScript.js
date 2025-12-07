58e3aff8c8ff728ed195c53091c9b624297fa33f
Evan Stone <StoneE5936@gmail.com>
2025-12-07 15:46:03 -0600

    Giving javascript logic to MSUBears.html

---
+++ prompts/MSUBears_javaScript.js
@@ -0,0 +1,11 @@
+for (let i = 1; i < 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log("MSUBears");
+    } else if (i % 5 === 0) {
+        console.log("Bears");
+    } else if (i % 3 === 0) {
+        console.log("MSU");
+    } else {
+        console.log(i);
+    }
+} 
