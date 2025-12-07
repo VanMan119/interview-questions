9419d0d80a22a0959e5e6f92bf3e2ab2598b01f4
Evan Stone <StoneE5936@gmail.com>
2025-12-07 17:38:15 -0600

    Fixed Fibonacci Logic

--- prompts/Fibonacci.js
+++ prompts/Fibonacci.js
@@ -1,16 +1,28 @@
 const input = document.getElementById("fib_input");
 const button = document.getElementById("fib_button");
-const output = document.getElementById("Fib_output");
+const output = document.getElementById("fib_output");
 
 function fibonacci(n) {
-    return 1;
+    let a = 1;
+    let b = 1;
+
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    for (let i = 2; i <= n; i++) {
+        let x = a + b;
+        a = b;
+        b = x;
+    }
+    return b;
 }
 
 button.addEventListener("click", () => {
     const input_value = input.value;
     const num = Number(input_value)
     if (!isNaN(num)) {
-        fib_number = fibonacci(value);
+        fib_number = fibonacci(num);
         output.textContent = "Your Fibonacci Number: " + fib_number
     }
     else {
